9ac734133560c9a2e9ff01f819d696033ec62f8f
Naman Arora <namanarora311@gmail.com>
2022-12-19 12:25:23 +0530

    toggle hide and show functionality

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -22,7 +22,13 @@ app.on("ready", () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
   tray = new Tray(iconPath);
   tray.on('click', ()=>{
-    mainWindow.show();
+    if(mainWindow.isVisible()){
+
+      mainWindow.hide();
+    }else{
+      mainWindow.show();
+    }
+   
   })
 
 });
